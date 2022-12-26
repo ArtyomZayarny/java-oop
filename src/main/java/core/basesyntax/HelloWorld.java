6549743dae94edeb17fe290d00ca6987406badb2
Artem Zaiarnyi <artem_zaiarnyi@epam.com>
2022-12-26 13:24:02 +0200

    Added classes and override methods

--- src/main/java/core/basesyntax/HelloWorld.java
+++ src/main/java/core/basesyntax/HelloWorld.java
@@ -3,6 +3,56 @@ package core.basesyntax;
 /**
  * Feel free to remove this class and create your own.
  */
-public class HelloWorld {
+public class Main {
+    Truck truck = new Truck();
+    Bulldozer bulldozer = new Bulldozer();
+    Excavator excavator = new Excavator();
+    Machine [] machines = {truck,bulldozer,excavator};
 
+    for(Machine machine: machines) {
+        System.out.println(machine.stopWork());
+        System.out.println(machine.doWork());
+    }
+
+    public class Machine {
+        public abstract void doWork() {
+
+        }
+
+        public abstract void stopWork() {
+
+        }
+    }
+
+    public class Truck extends Machine {
+        @Override
+        public abstract void doWork() {
+            return "Truck started its work";
+        }
+        @Override
+        public abstract void stopWork() {
+            return "BulTruckldozer stoped its work";
+        }
+    }
+    public class Bulldozer extends Machine {
+        @Override
+        public abstract void doWork() {
+            return "Bulldozer started its work";
+        }
+        @Override
+        public abstract void stopWork() {
+            return "Bulldozer stoped its work";
+        }
+    }
+
+    public class Excavator extends Machine {
+        @Override
+        public abstract void doWork() {
+            return "Excavator started its work";
+        }
+        @Override
+        public abstract void stopWork() {
+            return "Excavator stoped its work";
+        }
+    }
 }
